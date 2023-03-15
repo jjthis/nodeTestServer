3466bc48932ece54fd757a2ed4131ab2d21b5704
jthis <dfd2726@gmail.com>
2023-03-15 14:05:55 +0900

    design ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
-import "firebase/compat/firestore";
+import React, { createRef, useEffect, useState } from 'react'
 import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 import './App.css';
-import styled from "styled-components"
 
-import { useCollection, useCollectionData } from 'react-firebase-hooks/firestore';
 firebase.initializeApp({
 	apiKey: process.env.REACT_APP_apiKey,
 	authDomain: process.env.REACT_APP_authDomain,
@@ -16,65 +14,79 @@ firebase.initializeApp({
 });
 const firestore = firebase.firestore();
 
-const TopDiv = styled.div`
-	background-color: black;
-`;
-
 function App() {
-
 	return (
-		<TopDiv>
-			<div className="App">
-
-
-				{<ChatView />}
+		<>
+			 <div style={{textAlign: 'center'}}>
+				<h1> Chat </h1>
 			</div>
-		</TopDiv>
+			<div className="Tops">
+				<div className="App">
+					{<ChatView />}
+				</div>
+			</div>
+			<div className="blanks" />
+		</>
 	)
 }
-let showCnt = 25;
 
+let snap = () => { };
 function ChatView() {
-	const dummy = useRef();
-
-	console.log(showCnt);
+	const dummy = createRef();
+	const [showCnt, setShowCnt] = useState(5);
+	const [formValue, setFormValue] = useState('');
+	const [messages, setMessages] = useState('');
 
 	const chat = firestore.collection("chat");
 	const query = chat.orderBy('createdAt').limitToLast(showCnt);
-	const [snap] = useCollection(query, { idField: 'id' });
-	const [messages] = useCollectionData(query, { idField: 'id' });
-	const [formValue, setFormValue] = useState('');
-
-	const sendMessage = (e) => {
+	const addCount = (e) => {
 		e.preventDefault();
+		setShowCnt(showCnt + 5);
+	}
 
+	useEffect(() => {
+		query.get().then(doc => {
+			setMessages(doc.docs);
+		});
+		snap();
+		snap = query.onSnapshot(doc => {
+			setMessages(doc.docs);
+		});
+	}, [query]);
 
+	const sendMessage = async (e) => {
+		e.preventDefault();
 		chat.add({
 			text: formValue,
 			createdAt: firebase.firestore.FieldValue.serverTimestamp()
-		})
+		});
 
+		setShowCnt(showCnt + 1);
 		setFormValue('');
-		dummy.current.scrollIntoView({ behavior: 'smooth' });
+		window.scroll({
+			top: document.body.scrollHeight,
+			left: 0,
+			behavior: 'smooth',
+		});
+		// dummy.current.scrollIntoView({ behavior: "smooth" });
 	};
 	return (<>
-
+		<form onSubmit={addCount}>
+			<button type="submit">more 5</button>
+		</form>
 		<main>
-
-			{messages && messages.map((msg, idx) => <ChatMessage key={snap.docs[idx].id} keys={snap.docs[idx].id} message={msg} />)}
-
-			<span ref={dummy}></span>
-
+			{messages &&
+				messages.map((msg, idx) =>
+					<ChatMessage
+						key={msg.id} keys={msg.id} message={msg.data()} />)}
 		</main>
 
 		<form onSubmit={sendMessage}>
-
 			<input value={formValue} onChange={(e) => setFormValue(e.target.value)}
 				placeholder="input text" />
-
 			<button type="submit" disabled={!formValue}>send</button>
-
 		</form>
+		<div ref={dummy} />
 	</>)
 }
 
@@ -94,6 +106,5 @@ function ChatMessage(props) {
 		</div>
 	</>)
 }
-
 
 export default App;
